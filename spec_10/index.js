6d06003f4264061aa3de8d316085f0cba65b3efe
Ian Deans <ideans715@gmail.com>
2017-02-02 17:19:40 -0800

    $argv

--- spec_10/index.js
+++ spec_10/index.js
@@ -1,11 +1,15 @@
 
 const input =
-`
-3
+`3
 /bin:/usr/bin
 /usr/bin:/usr/local/bin/
 /usr/local/bin/log:/var/log-2017
-/bin/log/lib
-`
+/bin/log/lib`
+const splitInput = input.split('\n')
 
-console.log( input )
+const indexToResolve = parseInt( splitInput.shift() )
+
+const pathToBeResolved = splitInput[ indexToResolve ]
+
+
+console.log( pathToBeResolved )
